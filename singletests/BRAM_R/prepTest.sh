c4cdba908a1873218af6cf2dce4981e25a05f3a2
Brent Nelson <nelson@ee.byu.edu>
2020-05-29 17:48:33 -0600

    Modified instructions for running the tools.
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

--- singletests/BRAM_R/prepTest.sh
+++ singletests/BRAM_R/prepTest.sh
@@ -2,4 +2,7 @@
 
 D="128kb16"
 SIZES="16 128k 131072"
+# Generate the design, then patch the real.fasm with the contents of alt.mem to get an alt.fasm.  
+# This is essentially what generate_tests.py does but this lets us put the results where we want and allows us to name the files what we want.
+# Run them separated by && so if one or the other fails, this whole script will fail.
 ../../testing/generate_tests_script.sh . $SIZES  allones && python $MEM_PATCH_DIR/patch_mem.py $D/real.fasm $D/init/alt.mem $D/mapping.mdd $D/alt.fasm mem/ram
