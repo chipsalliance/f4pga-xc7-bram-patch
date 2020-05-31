942cb644f91f29448ba9ad7168d1d700d0248589
Brent Nelson <nelson@ee.byu.edu>
2020-05-30 22:21:49 -0600

    Added singletests/simple test case.
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

---
+++ singletests/simple/prepTest.sh
@@ -0,0 +1,1 @@
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 1 128 128
---
+++ singletests/simple/runTest.sh
@@ -0,0 +1,5 @@
+python $MEM_PATCH_DIR/run_tests.py 128b1
+
+# The above line of code does essentially what the following would do:
+# python $MEM_PATCH_DIR/run_tests.py 128b1/alt.fasm 128b1/init/init.mem 128b1/mapping.mdd 128b1/patched.fasm 128b1/real.fasm mem/ram
+#
