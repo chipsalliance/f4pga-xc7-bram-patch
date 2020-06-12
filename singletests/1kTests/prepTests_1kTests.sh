8644348c9346d4212062cfbd8dda5503ecba7b63
Brent Nelson <nelson@ee.byu.edu>
2020-06-12 07:53:01 -0600

    Test to determine layouts for sample designs.
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

---
+++ singletests/1kTests/prepTests_1kTests.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  1 1k  1024 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  2 1k  2048 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  3 1k  3072 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  4 1k  4096 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  5 1k  5120 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  6 1k  6144 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  7 1k  7168 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  8 1k  8192 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh .  9 1k  9216 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 10 1k 10240 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 11 1k 11264 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 12 1k 12288 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 13 1k 13312 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 14 1k 14336 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 15 1k 15360 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 16 1k 16384 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 17 1k 17408 allones
+$MEM_PATCH_DIR/testing/generate_tests_script.sh . 18 1k 18432 allones
---
+++ singletests/1kTests/summarize.py
@@ -0,0 +1,31 @@
+﻿import sys
+import os
+import glob
+import json
+
+dirs = glob.glob("1kb*")
+dirs.sort()
+for d in dirs:
+    print("########################################################################")
+    print(d)
+    try:
+        with open(d+"/"+d+".mdd.json") as f:
+            dat = json.load(f)
+            print(len(dat))
+            for d in dat:
+                print("{} {} {} {} {}:{} {}:{}".format(
+                    d['DESIGN'], 
+                    d["CELLTYPE"],
+                    d["MEM.PORTA.DATA_BIT_LAYOUT"], 
+                    d["READ_WIDTH_A"],
+                    d["BRAM_ADDR_END"],
+                    d["BRAM_ADDR_BEGIN"],
+                    d["BRAM_SLICE_END"],
+                    d["BRAM_SLICE_BEGIN"]
+                    )
+                )
+    except:
+        print("No JSON file found: {}".format(d))
+print("########################################################################")
+
+
