cd38318b3cbeace70c480b0d843af852f64b4995
Brent Nelson <nelson@ee.byu.edu>
2020-06-15 15:09:06 -0600

    Pulled code out to make readAndFilterMDDData().
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

--- patch_mem.py
+++ patch_mem.py
@@ -10,15 +10,7 @@ import cProfile
 from prjxray.db import Database
 from prjxray import fasm_disassembler
 
-
-def patch_mem(
-    fasm=None, init=None, mdd=None, outfile=None, selectedMemToPatch=None
-):
-    assert fasm is not None
-    assert init is not None
-    assert mdd is not None
-    assert selectedMemToPatch is not None
-
+def readAndFilterMDDData(mdd, selectedMemToPatch):
     # Read and filter the MDD file contents based on selectedMemToPatch
     tmp_mdd_data = mddutil.read_mdd(mdd)
     mdd_data = [
@@ -39,6 +31,19 @@ def patch_mem(
     for l in mdd_data:
         print("  " + l.toString())
     print("")
+    return mdd_data
+
+
+def patch_mem(
+    fasm=None, init=None, mdd=None, outfile=None, selectedMemToPatch=None
+):
+    assert fasm is not None
+    assert init is not None
+    assert mdd is not None
+    assert selectedMemToPatch is not None
+
+    
+    mdd_data = readAndFilterMDDData(mdd, selectedMemToPatch)
 
     # Get all the FASM tuples
     fasm_tups = read_fasm(fasm)
