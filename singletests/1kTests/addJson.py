18bffd47e369542ff15469b21a331a959dd074dd
Brent Nelson <nelson@ee.byu.edu>
2020-06-15 15:11:19 -0600

    Code to add/create json files
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

---
+++ singletests/1kTests/addJson.py
@@ -0,0 +1,10 @@
+﻿import glob
+import json
+
+dirs = glob.glob("/home/nelson/mempatch/testing/tests/master/*")
+dirs.sort()
+s = "source /home/nelson/mempatch/testing/mdd_json_add.tcl\n"
+for d in dirs:
+    des = d.split("/")[-1]
+    s += "cd {}\nopen_checkpoint vivado/{}.dcp\nmddMake\nclose_project\n".format(d, des)
+print(s)
