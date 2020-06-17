7433f0f1df683421a25cfc8a605558a341e14319
Brent Nelson <nelson@ee.byu.edu>
2020-06-16 18:41:49 -0600

    Adding documentation to code
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>
    
    Added documentation on findTheBits.py

--- findTheBits_xx.py
+++ findTheBits_xx.py
@@ -7,6 +7,7 @@
 #    So, you can run this and if no exceptions are thrown because all bits matched.
 
 import os
+import sys
 import glob
 import parseutil
 import argparse
@@ -17,27 +18,30 @@ def findAllBits(
 ):
 
     # Flag of whether this is RAMB36E cell or not
-    r36 = (cell.type == "RAMB36E1")
+    ramb36 = (cell.type == "RAMB36E1")
 
     # Step 1: Read the init.mem file for this design
+    # Put the contents into an array of strings
     initMemContents = parseutil.parse_init_test.initfile_to_initlist(
         initFile, mdd_data
     )
-    # Step 2: Read the fasm file for this design and collect the INIT lines, they should be in ascending order
-    if r36:
+    # Step 2: Read the fasm file for this design and collect the INIT lines for this cell.
+    if ramb36:  # This is for RAMB36E1's
         # Get all the INIT lines
-        init0lines, init0plines, init1lines, init1plines = collectInitStrings(
+        init0lines, init0plines, init1lines, init1plines = readInitStringsFromFASMFile(
             cell, fasmFile
         )
 
         # Convert those into the proper format strings
-        init0s = processInitLines(cell, init0lines)
-        init0ps = processInitLines(cell, init0plines)
-        init1s = processInitLines(cell, init1lines)
-        init1ps = processInitLines(cell, init1plines)
+        init0s = processInitLines(init0lines, False)
+        init0ps = processInitLines(init0plines, True)
+        init1s = processInitLines(init1lines, False)
+        init1ps = processInitLines(init1plines, True)
 
-        # Now zip them together into big 512 character lines
-        # That is, the even characters will come from Y0 and the odd from Y1
+        # Now zip the Y0 and Y1 bits together into big 512 character lines.
+        # The even characters come from Y0 and the odd from Y1
+        # Once this is done the memory can be handled pretty much
+        # like the RAMB18E1 ones in the later code below.
         inits = []
         for i in range(len(init0s)):
             itm = ""
@@ -50,21 +54,27 @@ def findAllBits(
             for j in range(256):
                 itm = itm + init0ps[i][j] + init1ps[i][j]
             initps.append(itm)
-    else:  # !r36
-        # Get all the INIT lines
-        # Only the init0 and init0p should have anything since this is a RAMB18E1 primitive
-        init0lines, init0plines, init1lines, init1plines = collectInitStrings(
+    else:  # !ramb36 - This is for RAMB18E1's
+        # Read all the INIT lines from the FASM file
+        init0lines, init0plines, init1lines, init1plines = readInitStringsFromFASMFile(
             cell, fasmFile
         )
-        assert len(init1lines) == 0 and len(init1plines) == 0, "{} {}".format(
-            len(init1lines), len(init1plines)
-        )
+        # Only the init0 and init0p should have anything since this is a RAMB18E1 primitive
+        assert len(init1lines) == 0, "{}".format(designName)
+        assert len(init1plines) == 0, "{}".format(designName)
 
         # Get all the init lines for this cell, pad them with 0's, and reverse them
-        inits = processInitLines(cell, init0lines)
-        initps = processInitLines(cell, init0plines)
+        inits = processInitLines(init0lines, False)
+        initps = processInitLines(init0plines, True)
 
     # Step 3: Now check if we can find all the bits in this cell
+    # We use the length of the init file to determine the # of words to check since
+    # for a small memory (128b1), Vivado will generate a larger one, knowing you will
+    # only use the lower part of the memory.
+    # NOTE: this doesn't quite what the description of the program says.
+    # The description says it will check and output the mapping for every bit in the init file.
+    # Technically, this checks and outputs every bit in every RAMB primitive making up your memory.
+    # Should be equivalent since all init file bits should be in the RAMB primitives, but there may be cases.
     for w in range(cell.addr_beg, cell.addr_beg + len(initMemContents)):
         for b in range(cell.slice_beg, cell.slice_end + 1):
             bwid = cell.slice_end - cell.slice_beg + 1
@@ -73,11 +83,17 @@ def findAllBits(
             pwid = cell.pbits
             dwid = cell.dbits
             if bwid != dwid + pwid:
-                print("     ERROR: {} {} {} {}".format(bwid, rwid, pwid, dwid))
+                print(
+                    "     BWID ERROR: {} {} {} {}".format(
+                        bwid, rwid, pwid, dwid
+                    ),
+                    file=sys.stderr
+                )
                 continue
-            # Chop up the bits between parity and normal bits
-            # Only the RAMB36E1 can have an rwid of 72
-            if not r36:
+
+            # Determine how many bits are in parity and how many are in the "normal" bits
+            # Also, only a RAMB36E1 can have an rwid of 72
+            if not ramb36:
                 assert not rwid == 72
             islice = 64 if rwid == 72 else 32 if rwid == 36 else 16 if rwid == 18 else 8 if rwid == 9 else rwid
             pslice = 8 if rwid == 72 else 4 if rwid == 36 else 2 if rwid == 18 else 1 if rwid == 9 else 0
@@ -111,12 +127,12 @@ def findAllBits(
                     )
                 )
             # Some sanity checks
-            # Number of bits in INIT and INITP should be 32K/16K and 4K respectively (r32) or 16K and 2K respectively
-            assert islice * wdep == (32768 if r36 else 16384)
+            # Number of bits in INIT and INITP should be 32K and 4K respectively (RAMB36E1) or 16K and 2K respectively (RAMB18E1)
+            assert islice * wdep == (32768 if ramb36 else 16384)
             if pslice != 0:
-                assert pslice * wdep == (4096 if r36 else 2048)
+                assert pslice * wdep == (4096 if ramb36 else 2048)
 
-            # Is the bit in the INIT portion of the INITP portion?
+            # Is the bit of interest in the INIT portion or the INITP portion?
             if b - cell.slice_beg < islice:
                 bslice = islice
                 parity = False
@@ -124,9 +140,16 @@ def findAllBits(
                 bslice = pslice
                 parity = True
 
-            # Computer how many bits make up each "word"
-            initLen = 512 if r36 else 256
+            # Compute how many "words" fit into each INIT string.
+            # This may be the width of the memory or it may not since
+            # Vivado often pads.  Example: for a 128b1 the "read width" is 18.
+            # That means the memory has a 16-bit word in the INIT and a 2-bit word in the INITP.
+            # In the INIT there are 15 0's as padding + the 1 bit of data.
+            initLen = 512 if ramb36 else 256
+
+            # How many words are in each INIT and INITP string?
             numPerInit = (initLen / bslice)
+            # Make sure it divides evenly or there must be a problem
             assert int(numPerInit) == numPerInit
 
             if verbose:
@@ -136,11 +159,13 @@ def findAllBits(
                         len(initMemContents[w]) - 1 - b, initMemContents[w]
                     )
                 )
-            # Get the bit from the .mem inititalization file array
+            # Pull the bit from the .mem inititalization file array
             initbit = initMemContents[w][len(initMemContents[w]) - 1 - b]
 
-            # Computer where to find the bit in the INIT strings (after they were combined and reversed above)
+            # Compute where to find the bit in the INIT strings (after they were combined and reversed above)
+            # Find which INIT or INITP it is
             initRow = int(w / numPerInit)
+            # Now, compute the actual bit offset into that INIT or INITP string
             wordOffset = int(w % numPerInit)
             bitOffset = wordOffset * bslice + (
                 b - cell.slice_beg - (islice if parity else 0)
@@ -151,9 +176,12 @@ def findAllBits(
                         initRow, bitOffset
                     )
                 )
-            # Read the bit from either the INIT or the INITP section
-            fasmbit = initps[initRow][bitOffset] if parity else inits[initRow][
-                bitOffset]
+            # Read the bit from either the INIT or the INITP word
+            if parity:
+                fasmbit = initps[initRow][bitOffset]
+            else:
+                fasmbit = inits[initRow][bitOffset]
+
             if verbose:
                 print("Bit[{}][{}] = {} vs. {}".format(w, b, initbit, fasmbit))
 
@@ -162,6 +190,8 @@ def findAllBits(
                 assert initbit == fasmbit
 
             # Finally, print out the mapping if requested
+            # The resulting string could be used to look up the actual frame/bit numbers from the
+            # prjxray database (the .../prjxray/database/artix7/segbits_bram_l.block_ram.db file)
             initRow = "{:02x}".format(initRow).upper()
             if mappings or verbose:
                 if parity:
@@ -173,10 +203,10 @@ def findAllBits(
                             b,
                             cell.tile[0:6],
                             cell.type[:-2],
-                            '0' if (r36 is False or bitOffset % 2 == 0) else
+                            '0' if (ramb36 is False or bitOffset % 2 == 0) else
                             '1',  # change
                             initRow,
-                            int(bitOffset / 2) if r36 else bitOffset
+                            int(bitOffset / 2) if ramb36 else bitOffset
                         )
                     )
                 else:
@@ -188,13 +218,14 @@ def findAllBits(
                             b,
                             cell.tile[0:6],
                             cell.type[:-2],
-                            '0' if (r36 is False or bitOffset % 2 == 0) else
+                            '0' if (ramb36 is False or bitOffset % 2 == 0) else
                             '1',  # change
                             initRow,
-                            int(bitOffset / 2) if r36 else bitOffset
+                            int(bitOffset / 2) if ramb36 else bitOffset
                         )
                     )
-    # If we got here, it worked so say so...
+    # If we got here, it worked.
+    # So say so if you were asked to...
     if check:
         print(
             "    Cell: {} {} {} all checked out and correct...".format(
@@ -204,13 +235,15 @@ def findAllBits(
         )
 
 
+# Pad a string to a certain length with 'ch'
 def pad(ch, wid, data):
     tmp = str(data)
     return (ch * (wid - len(tmp)) + tmp)
 
 
-# Read the FASM file and filter out Y0 and Y1 INIT and INITP strings and put into lists to return
-def collectInitStrings(cell, fasmFile):
+# Read the FASM file and filter out Y0 and Y1 INIT and INITP strings
+# for the current cell and put into lists to return.
+def readInitStringsFromFASMFile(cell, fasmFile):
     init0lines = []
     init0plines = []
     init1lines = []
@@ -230,20 +263,26 @@ def collectInitStrings(cell, fasmFile):
     return (init0lines, init0plines, init1lines, init1plines)
 
 
-# Read the INIT lines one at a time
-# Pad them into 256 character lines and reverse them end to end
-# Return a list of them
-def processInitLines(cell, initlines):
+# Process the INIT lines one at a time.
+# Pad them into 256 character lines and reverse them end to end.
+# Return a list of them.
+# They should appear in ascending order, this checks that.
+# TODO: is it possible to have less than the full count of 64 INIT lines?
+#           Yes: see design 512b18.
+#       Would an INIT get left out of the FASM file it is was all zeroes?
+#           It is possible - but haven't seen this yet since using random data.
+#       To be safe, filling out full complement of lines with 0's in the code below.
+#       May be overkill but hey...
+def processInitLines(initlines, parity):
     inits = []
-    i = 0
-    for line in initlines:
-        if line.split(".")[0] != cell.tile:
-            continue
+    for indx, line in enumerate(initlines):
         key = line.split(".")[2].split("_")[1][0:2]
         key = int(key, 16)
-        assert key == i
+        assert key == indx
         val = line.split("'")[1][1:].rstrip()
         val = pad('0', 256, val)[::-1]
         inits.append(val)
-        i += 1
+    while len(inits) < (8 if parity else 64):
+        inits.append("0" * 256)
+
     return inits
