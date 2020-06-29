a6ce784e13c565b4b91097f4ac42c83d6d6f55f4
Brent Nelson <nelson@ee.byu.edu>
2020-06-29 15:56:45 -0600

    Whitespace
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

--- frame_pattern.py
+++ frame_pattern.py
@@ -5,6 +5,7 @@ import parseutil.parse_mdd as mddutil
 import patch_mem
 import json
 
+
 def make_2D_list(numberOfLists, numberOfEntriesInLists, isZeroes=False):
     outputList = []
     if isZeroes:
@@ -17,15 +18,10 @@ def make_2D_list(numberOfLists, numberOfEntriesInLists, isZeroes=False):
             outputList.append(listEntry)
     return outputList
 
+
 def frame_pattern(
-    fasmFile,
-    dbFile,
-    memoryToRead,
-    mddFile,
-    jsonFile,
-    frmFile,
-    outfile
-    ):
+    fasmFile, dbFile, memoryToRead, mddFile, jsonFile, frmFile, outfile
+):
 
     WORD_LENGTH = 8
     output = {}
@@ -41,24 +37,32 @@ def frame_pattern(
     with open(fasmFile, 'r') as f:
         fasmLines = f.readlines()
 
-    # Loop through the data multiple times, each time creating data relating to a specific tile. 
+    # Loop through the data multiple times, each time creating data relating to a specific tile.
     # Once the tile's frame data has been generated, add all of the data to a dictionary called output.
     for mddEntry in mddData:
         isRAMB36 = False
         initPStrings = []
         basicInitStrings = []
-        
+
         # Loop through the FASM lines, and filter out any that are not INIT, or INITP relating to the current tile
         # Sets a flag "isRAMB36", which tells the software to make slight changes later to accomadate the extra data
         for segDataLine in fasmLines:
-            if re.search(mddEntry.tile + "\\.", segDataLine) != None and re.search("\.INIT_", segDataLine) != None:
-                hexStringList = list(re.search("\'.*", segDataLine).group(0)[2:])
+            if re.search(mddEntry.tile + "\\.",
+                         segDataLine) != None and re.search(
+                             "\.INIT_", segDataLine) != None:
+                hexStringList = list(
+                    re.search("\'.*", segDataLine).group(0)[2:]
+                )
                 hexStringList.reverse()
                 basicInitStrings.append(hexStringList)
                 if re.search("Y1\.INIT_", segDataLine) != None:
                     isRAMB36 = True
-            elif re.search(mddEntry.tile + "\\.", segDataLine) != None and re.search("\.INITP_", segDataLine) != None:
-                hexStringList = list(re.search("\'.*", segDataLine).group(0)[2:])
+            elif re.search(mddEntry.tile + "\\.",
+                           segDataLine) != None and re.search(
+                               "\.INITP_", segDataLine) != None:
+                hexStringList = list(
+                    re.search("\'.*", segDataLine).group(0)[2:]
+                )
                 hexStringList.reverse()
                 initPStrings.append(hexStringList)
 
@@ -84,45 +88,55 @@ def frame_pattern(
                 bitIndex = int(re.search("\[...", segDataLine).group(0)[1:])
                 if initStringIndex < numOfInitStrings:
                     if BRAMSide == "Y1" and isRAMB36:
-                        basicSegData[initStringIndex + 64][bitIndex] = segDataLine
+                        basicSegData[initStringIndex +
+                                     64][bitIndex] = segDataLine
                     elif BRAMSide == "Y0":
                         basicSegData[initStringIndex][bitIndex] = segDataLine
             elif initPRegexResult != None:
                 BRAMSide = re.search("Y[01]", segDataLine).group(0)
-                initStringIndex = int(initPRegexResult.group(0)[7:].lower(), 16)
+                initStringIndex = int(
+                    initPRegexResult.group(0)[7:].lower(), 16
+                )
                 bitIndex = int(re.search("\[...", segDataLine).group(0)[1:])
                 if initStringIndex < len(initPStrings):
                     if BRAMSide == "Y1" and isRAMB36:
-                        initPSegData[initStringIndex + 8][bitIndex] = segDataLine
+                        initPSegData[initStringIndex +
+                                     8][bitIndex] = segDataLine
                     elif BRAMSide == "Y0":
-                        initPSegData[initStringIndex][bitIndex] = segDataLine        
+                        initPSegData[initStringIndex][bitIndex] = segDataLine
 
         reorderedBits = []
         if isRAMB36:
             reorderedBits = make_2D_list(numOfInitStrings, 320, True)
         else:
             reorderedBits = make_2D_list(numOfInitStrings * 2, 320, True)
-        
-        # Uses the init strings, and the segData from the db file to reorder the init strings into the 
+
+        # Uses the init strings, and the segData from the db file to reorder the init strings into the
         # Order used in the frm files
         for i in range(len(basicSegData)):
             for j in range(len(basicSegData[i])):
-                segDataRegexResult = re.search(" .*_.*", basicSegData[i][j]).group(0)
+                segDataRegexResult = re.search(" .*_.*",
+                                               basicSegData[i][j]).group(0)
                 dataBreak = segDataRegexResult.find('_')
                 initStringIndex = int(segDataRegexResult[1:dataBreak])
                 bitIndex = int(segDataRegexResult[dataBreak + 1:])
                 if j < len(basicInitStrings[i]):
-                    reorderedBits[initStringIndex][bitIndex] = str(basicInitStrings[i][j])
+                    reorderedBits[initStringIndex][bitIndex] = str(
+                        basicInitStrings[i][j]
+                    )
 
         # Same as above but done for the INITP strings
         for i in range(len(initPSegData)):
             for j in range(len(initPSegData[i])):
-                segDataRegexResult = re.search(" .*_.*", initPSegData[i][j]).group(0)
+                segDataRegexResult = re.search(" .*_.*",
+                                               initPSegData[i][j]).group(0)
                 dataBreak = segDataRegexResult.find('_')
                 initStringIndex = int(segDataRegexResult[1:dataBreak])
                 bitIndex = int(segDataRegexResult[dataBreak + 1:])
                 if j < len(initPStrings[i]):
-                    reorderedBits[initStringIndex][bitIndex] = str(initPStrings[i][j])
+                    reorderedBits[initStringIndex][bitIndex] = str(
+                        initPStrings[i][j]
+                    )
 
         # Takes the 2D array of binary output from above, and converts it into a 2D list of Hexadecimal 32 bit words
         frameStrings = []
@@ -133,9 +147,14 @@ def frame_pattern(
                 if i == 0:
                     break
                 elif i < WORD_LENGTH:
-                    hexStringList.append("0x" + ("0" * (WORD_LENGTH - i)) + hexString[0:i].upper())
+                    hexStringList.append(
+                        "0x" + ("0" * (WORD_LENGTH - i)) +
+                        hexString[0:i].upper()
+                    )
                 else:
-                    hexStringList.append("0x" + hexString[i - WORD_LENGTH: i].upper())
+                    hexStringList.append(
+                        "0x" + hexString[i - WORD_LENGTH:i].upper()
+                    )
             frameStrings.append(hexStringList)
 
         with open(jsonFile) as f:
@@ -160,7 +179,7 @@ def frame_pattern(
         for i in range(bramTile["frames"]):
             offset = 11 * (bramTile["offset"] + 1)
             for j in range(bramTile["words"]):
-                data = frmData[frmFilePointer][offset:offset+10]
+                data = frmData[frmFilePointer][offset:offset + 10]
                 offset += 11
                 if data == "0x00000000" or data == frameStrings[i][j]:
                     continue
@@ -174,10 +193,16 @@ def frame_pattern(
                 break
         if failFlag:
             writeFile.write("Output and frame file mismatch\n")
-            writeFile.write("Expected Token: {}, Received Token: {}\n".format(data, frameStrings[failIndex][failOffset]))
+            writeFile.write(
+                "Expected Token: {}, Received Token: {}\n".format(
+                    data, frameStrings[failIndex][failOffset]
+                )
+            )
             failHex = hex(int(bramTile["baseaddr"], 16) + failIndex)[2:]
             failAddress = "0x" + ("0" * (WORD_LENGTH - len(failHex))) + failHex
-            writeFile.write("Location at X: {}, Y: {}\n".format(failAddress, failOffset))
+            writeFile.write(
+                "Location at X: {}, Y: {}\n".format(failAddress, failOffset)
+            )
             testsWorked = False
         else:
             writeFile.write("Output and Expected match in all instances\n")
@@ -185,8 +210,12 @@ def frame_pattern(
         # Outputs the hexadecimal that we generated to the outfile
         for i in range(len(frameStrings)):
             addressHex = hex(int(bramTile["baseaddr"], 16) + i)[2:]
-            formattedAddress = "0x" + ("0" * (WORD_LENGTH - len(addressHex))) + addressHex
-            writeFile.write("{}:\t{}\n".format(formattedAddress, frameStrings[i]))
+            formattedAddress = "0x" + (
+                "0" * (WORD_LENGTH - len(addressHex))
+            ) + addressHex
+            writeFile.write(
+                "{}:\t{}\n".format(formattedAddress, frameStrings[i])
+            )
         writeFile.write("\n\n")
 
         # Actually writes the hexadecimal to a dictionary keyed by the tile name
@@ -199,8 +228,14 @@ def frame_pattern(
 
     writeFile.close()
 
+
 if __name__ == '__main__':
     if len(sys.argv) == 8:
-        frame_pattern(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+        frame_pattern(
+            sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5],
+            sys.argv[6], sys.argv[7]
+        )
     else:
-        print("Usage: fasmFile dbFile memoryToRead mddFile jsonFile frmFile outfile")
+        print(
+            "Usage: fasmFile dbFile memoryToRead mddFile jsonFile frmFile outfile"
+        )
