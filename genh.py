fa5fe7723784a632422a56d8c1dc46a051e9a1f2
Brent Nelson <nelson@ee.byu.edu>
2020-07-02 10:48:56 -0600

    Added comments
    Signed-off-by: Brent Nelson <nelson@ee.byu.edu>

--- genh.py
+++ genh.py
@@ -89,10 +89,13 @@ def getMDDMemories(mddName):
     mdd = parseutil.parse_mdd.read_mdd(mddName)
     lst = dict()
     for m in mdd:
+        # Create memory name from cell_name and ram_name
         s = '/'.join(m.cell_name.split('/')[:-1]) + '/' + m.ram_name
+        # Add it to list if not there already
         if s not in lst.keys():
             lst[s] = [int(m.addr_end) + 1, int(m.slice_end) + 1]
         else:
+            # Update coordinates so we know how big the memory is
             itm = lst[s]
             wb = [0, 0]
             wb[0] = max(itm[0], int(m.addr_end) + 1)
